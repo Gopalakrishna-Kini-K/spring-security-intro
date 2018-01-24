f80739e7aa462438ad8d6bd00c814ab4bf5f1656
Gopal Krishna Kini <gopal@caliberassurance.com>
2018-01-24 23:01:12 +0530

    Now spring boot starter is set to 1.5.9

---
+++ src/main/java/com/strugglingCoder/springsecurityintro/SpringSecurityIntroApplication.java
@@ -0,0 +1,12 @@
+package com.strugglingCoder.springsecurityintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityIntroApplication.class, args);
+	}
+}
