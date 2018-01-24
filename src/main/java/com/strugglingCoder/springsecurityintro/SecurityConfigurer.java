ff5b2c5a8c6b32903bb5c429fffb64066a7ecf20
Gopal Krishna Kini <gopal@caliberassurance.com>
2018-01-25 01:06:28 +0530

    Default spring boot security user overriden. In memory user added. Log in working with one

--- src/main/java/com/strugglingCoder/springsecurityintro/SecurityConfigurer.java
+++ src/main/java/com/strugglingCoder/springsecurityintro/SecurityConfigurer.java
@@ -1,14 +1,52 @@
 package com.strugglingCoder.springsecurityintro;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * Created by Kini on 25-Jan-18.
  */
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+    //create some users for demo purposes
+
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+        auth.inMemoryAuthentication()
+                .withUser("bruce").password("iambatman")
+                .roles("admin").and()
+                .withUser("clark").password("iamsuperman")
+                .roles("user").and()
+                .withUser("tony").password("iamironman")
+                .roles("technician");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin/*").hasRole("admin")
+                .antMatchers("/user/*").hasRole("user")
+                .antMatchers("/tech/*").hasRole("technician")
+                .antMatchers("/hello").permitAll()
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        // ALTOUGH THIS SEEMS LIKE USELESS CODE,
+        // ITS REQUIRED TO PREVEND SPRING BOOT AUTO-CONFIGURATION
+        return super.authenticationManagerBean();
     }
 }
--- src/main/java/com/strugglingCoder/springsecurityintro/SpringSecurityIntroApplication.java
+++ src/main/java/com/strugglingCoder/springsecurityintro/SpringSecurityIntroApplication.java
@@ -2,6 +2,8 @@ package com.strugglingCoder.springsecurityintro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringSecurityIntroApplication {
@@ -10,3 +12,27 @@ public class SpringSecurityIntroApplication {
 		SpringApplication.run(SpringSecurityIntroApplication.class, args);
 	}
 }
+
+@RestController
+class HelloWorldController {
+    @GetMapping(value ="/hello")
+    public String helloWorld(){
+        return "Hello";
+    }
+
+    @GetMapping(value="/admin/hello")
+    public String adminHello(){
+        return "Hello Admin";
+    }
+
+    @GetMapping(value = "/user/hello")
+    public String userHello(){
+        return "User hello!";
+    }
+
+    @GetMapping(value="/technician/hello")
+    public String techHello(){
+        return "Tech hello";
+    }
+}
+
